d150186de0b1fd0cfd651d0fb11c38768b13e18d
y4shiro <y4shironao@gmail.com>
2021-05-23 11:40:24 +0900

    BookRows コンポーネントを実装し、本一覧を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
-import React from "react";
-import "./App.css";
-import { BookToRead } from "./BookToRead";
+import React from 'react';
+import './App.css';
+import { BookToRead } from './BookToRead';
+import BookRow from './BookRow';
+import { idText } from 'typescript';
 
 const dummyBooks: BookToRead[] = [
   {
     id: 1,
-    title: "はじめてのReact",
-    authors: "ダミー",
-    memo: ""
+    title: 'はじめてのReact',
+    authors: 'ダミー',
+    memo: '',
   },
   {
     id: 2,
-    title: "React Hooks入門",
-    authors: "ダミー",
-    memo: ""
+    title: 'React Hooks入門',
+    authors: 'ダミー',
+    memo: '',
   },
   {
     id: 3,
-    title: "実践Reactアプリケーション開発",
-    authors: "ダミー",
-    memo: ""
-  }
+    title: '実践Reactアプリケーション開発',
+    authors: 'ダミー',
+    memo: '',
+  },
 ];
 
 const App = () => {
+  const bookRows = dummyBooks.map((book) => {
+    return (
+      <BookRow
+        book={book}
+        key={book.id}
+        onMemoChange={(id, memo) => {}}
+        onDelete={(id) => {}}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <section className="nav">
         <h1>読みたい本リスト</h1>
         <div className="button-like">本を追加</div>
       </section>
-      <section className="main">
-        <h1>チュートリアルを始めましょう</h1>
-      </section>
+      <section className="main">{bookRows}</section>
     </div>
   );
 };
